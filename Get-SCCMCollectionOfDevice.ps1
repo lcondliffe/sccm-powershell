0a063a9af2708248f48a4e1875f7e7c19a2fe757
Luke Williams <zz-782824@derby.ac.uk>
2019-08-15 09:44:39 +0100

    Added 'Get-SCCMCollectionofDevice Function

---
+++ Get-SCCMCollectionOfDevice.ps1
@@ -0,0 +1,58 @@
+﻿function Get-SCCMCollectionOfDevice
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Computername
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [String]$Computer,
+
+        [String]$SiteCode = "TST",
+
+        [String]$SiteServer = "sccm-prd-ss01"
+    )
+Begin
+{
+    [string] $Namespace = "root\SMS\site_$SiteCode"
+}
+
+Process
+{
+    $si=1
+    Write-Progress -Activity "Retrieving ResourceID for computer $computer" -Status "Retrieving data" 
+    $ResIDQuery = Get-WmiObject -ComputerName $SiteServer -Namespace $Namespace -Class "SMS_R_SYSTEM" -Filter "Name='$Computer'"
+    
+    If ([string]::IsNullOrEmpty($ResIDQuery))
+    {
+        Write-Output "System $Computer does not exist in Site $SiteCode"
+    }
+    Else
+    {
+    $Collections = (Get-WmiObject -ComputerName $SiteServer -Class sms_fullcollectionmembership -Namespace $Namespace -Filter "ResourceID = '$($ResIDQuery.ResourceId)'")
+    $colcount = $Collections.Count
+    
+    $devicecollections = @()
+    ForEach ($res in $collections)
+    {
+        $colid = $res.CollectionID
+        Write-Progress -Activity "Processing  $si / $colcount" -Status "Retrieving Collection data" -PercentComplete (($si / $colcount) * 100)
+
+        $collectioninfo = Get-WmiObject -ComputerName $SiteServer -Namespace $Namespace -Class "SMS_Collection" -Filter "CollectionID='$colid'"
+        $object = New-Object -TypeName PSObject
+        $object | Add-Member -MemberType NoteProperty -Name "CollectionID" -Value $collectioninfo.CollectionID
+        $object | Add-Member -MemberType NoteProperty -Name "Name" -Value $collectioninfo.Name
+        $object | Add-Member -MemberType NoteProperty -Name "Comment" -Value $collectioninfo.Comment
+        $object | Add-Member -MemberType NoteProperty -Name "LastRefreshTime" -Value ([Management.ManagementDateTimeConverter]::ToDateTime($collectioninfo.LastRefreshTime))
+        $devicecollections += $object
+        $si++
+    }
+} # end check system exists
+}
+
+End
+{
+    return $devicecollections
+}
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Get-SCCMAppDeploymentStatus - Query for a summary of a deployment or return compliant/non-compliant device lists for further action.
 Start-SCCMAppInstallation - Force a client to start an SCCM app installation assigned to it.
+Get-SCCMCollectionOfDevice - Get the collection memberships of a device.
